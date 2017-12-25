ff444bb9f7c9364a5a758b3cc3d62886605bb316
SmartinUU <1533204193@qq.com>
2017-12-25 17:20:23 +0800

    欢迎界面逻辑代码

--- app/src/main/java/cn/edu/gdme/android/boxuegu/MainActivity.java
+++ app/src/main/java/cn/edu/gdme/android/boxuegu/MainActivity.java
@@ -1,10 +1,8 @@
 package cn.edu.gdme.android.boxuegu;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
