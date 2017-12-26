6dd34b964de0115d8aa288ab206c3a876adac530
SmartinUU <1533204193@qq.com>
2017-12-26 13:22:17 +0800

    更改包名1

---
+++ app/src/main/java/cn/edu/gdme/android/boxuegu/MainActivity.java
@@ -0,0 +1,11 @@
+package cn.edu.gdme.android.boxuegu;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
