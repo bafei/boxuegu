e7a327b30378b22ba9cd4dff5dde746e98f79424
SmartinUU <1533204193@qq.com>
2017-12-26 17:10:30 +0800

    第四章底部状态栏+注册/登录微调

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/MainActivity.java
@@ -2,7 +2,6 @@ package cn.edu.gdmec.android.boxuegu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import cn.edu.gdmec.android.boxuegu.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/LoginActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/LoginActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
