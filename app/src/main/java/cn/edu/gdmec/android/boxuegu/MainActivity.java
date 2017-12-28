62ddb710ba93945d0f84fdca5e629472047386c6
bafei <1037219755@qq.com>
2017-12-28 11:53:33 +0800

    习题部分 存在bug 待解决

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/MainActivity.java
@@ -1,11 +1,11 @@
 package cn.edu.gdmec.android.boxuegu;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -15,34 +15,35 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import cn.edu.gdmec.android.boxuegu.View.ExerciseView;
 import cn.edu.gdmec.android.boxuegu.View.MyInfoView;
 
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(data !=null){
-            boolean isLogin = data.getBooleanExtra("isLogin", false);
-            if(isLogin){ //登录成功后显示课程界面
+        if (data!=null){
+            boolean isLogin = data.getBooleanExtra("isLogin",false);
+            if (isLogin){//登录成功后显示课程界面
                 clearBottomImageState();
                 selectDisplayView(0);
             }
-            if(mMyInfoView !=null){
+            if (mMyInfoView!=null){
                 mMyInfoView.setLoginParams(isLogin);
             }
         }
     }
 
     private TextView tv_back;
-    private RelativeLayout rl_title_bar;
     private TextView tv_main_title;
+    private RelativeLayout rl_title_bar;
     private FrameLayout mBodyLayout;
-    private RelativeLayout mCourseBtn;
     private LinearLayout mBottomLayout;
+    private RelativeLayout mCourseBtn;
+    private TextView tv_course;
     private RelativeLayout mExercisesBtn;
     private RelativeLayout mMyInfoBtn;
-    private TextView tv_course;
     private TextView tv_exercises;
     private TextView tv_myInfo;
     private ImageView iv_course;
@@ -56,7 +57,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         init();
         initBottomBar();
         setListener();
-        setInitStatus();//设置默认进入主页的显示
+        setInitStatus();
     }
 
     private void setInitStatus() {
@@ -66,44 +67,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setListener() {
-        for (int i = 0; i < mBottomLayout.getChildCount(); i++) {
+        for (int i=0; i<mBottomLayout.getChildCount();i++){
             mBottomLayout.getChildAt(i).setOnClickListener(this);
         }
     }
 
     private void initBottomBar() {
         mBottomLayout = (LinearLayout) findViewById(R.id.main_bottom_bar);
-        /*底部导航栏的三个按钮，实为RelativeLayout组件*/
+
         mCourseBtn = (RelativeLayout) findViewById(R.id.bottom_bar_course_btn);
         mExercisesBtn = (RelativeLayout) findViewById(R.id.bottom_bar_exercise_btn);
         mMyInfoBtn = (RelativeLayout) findViewById(R.id.bottom_bar_myInfo_btn);
-        /*三个选项的具体部件*/
+
         tv_course = (TextView) findViewById(R.id.bottom_bar_text_course);
         tv_exercises = (TextView) findViewById(R.id.bottom_bar_text_exercises);
         tv_myInfo = (TextView) findViewById(R.id.bottom_bar_text_myInfo);
-        iv_course = ((ImageView) findViewById(R.id.bottom_bar_image_course));
-        iv_exercises = ((ImageView) findViewById(R.id.bottom_bar_image_exercises));
-        iv_myInfo = ((ImageView) findViewById(R.id.bottom_bar_image_myInfo));
+
+        iv_course = (ImageView)findViewById(R.id.bottom_bar_image_course);
+        iv_exercises = (ImageView)findViewById(R.id.bottom_bar_image_exercises);
+        iv_myInfo = (ImageView)findViewById(R.id.bottom_bar_image_myInfo);
     }
 
-    /*获取界面UI控件*/
     private void init() {
         tv_back = (TextView) findViewById(R.id.tv_back);
         tv_main_title = (TextView) findViewById(R.id.tv_main_title);
         tv_main_title.setText("博学谷课程");
         rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
-        rl_title_bar.setBackgroundColor(Color.parseColor("#30B4ff"));
+        rl_title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
         tv_back.setVisibility(View.GONE);
         initBodyLayout();
     }
 
     private void initBodyLayout() {
-        mBodyLayout = (FrameLayout) findViewById(R.id.main_body);
+        mBodyLayout = (FrameLayout)findViewById(R.id.main_body);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
+        switch (v.getId()){
             case R.id.bottom_bar_course_btn:
                 clearBottomImageState();
                 selectDisplayView(0);
@@ -119,57 +120,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    /*
-    * 显示对应的页面
-    * */
     private void selectDisplayView(int index) {
         removeAllView();
         createView(index);
         setSelectedStatus(index);
     }
-    /*
-    * 设置底部按钮的选中状态
-    * */
+
     private void setSelectedStatus(int index) {
         switch (index){
             case 0:
-                mCourseBtn.setSelected(true);  //设置为选用
-                iv_course.setImageResource(R.drawable.main_course_icon_selected );
+                //TODO：课程界面
+                mCourseBtn.setSelected(true);
+                iv_course.setImageResource(R.drawable.main_course_icon_selected);
                 tv_course.setTextColor(Color.parseColor("#0097f7"));
                 rl_title_bar.setVisibility(View.VISIBLE);
                 tv_main_title.setText("博学谷课程");
                 break;
             case 1:
                 mExercisesBtn.setSelected(true);
-                iv_exercises.setImageResource(R.drawable.main_exercises_icon_selected );
+                iv_exercises.setImageResource(R.drawable.main_exercises_icon_selected);
                 tv_exercises.setTextColor(Color.parseColor("#0097f7"));
                 rl_title_bar.setVisibility(View.VISIBLE);
                 tv_main_title.setText("博学谷习题");
                 break;
             case 2:
                 mMyInfoBtn.setSelected(true);
-                iv_myInfo.setImageResource(R.drawable.main_my_icon_selected );
+                iv_myInfo.setImageResource(R.drawable.main_my_icon_selected);
                 tv_myInfo.setTextColor(Color.parseColor("#0097f7"));
                 rl_title_bar.setVisibility(View.VISIBLE);
-                tv_main_title.setText("个人资料");
+                tv_main_title.setText("博学谷");
                 break;
         }
     }
+    private ExerciseView mExercisesView;
     private MyInfoView mMyInfoView;
-    /*
-    * 选择视图
-    * */
-    private void createView(int viewIndex) {
-        switch (viewIndex){
+    //选择视图
+    private void createView(int viewindex) {
+        switch (viewindex){
             case 0:
-                //TODO:课程界面
+                //课程部分
                 break;
             case 1:
-                //习题界面
+                if (mExercisesView == null){
+                    mExercisesView = new ExerciseView(this);
+                    mBodyLayout.addView(mExercisesView.getView());
+                }else{
+                    mExercisesView.getView();
+                }
+                mExercisesView.showView();
                 break;
             case 2:
-                //我的界面
-                if(mMyInfoView == null){
+                if (mMyInfoView == null){
                     mMyInfoView = new MyInfoView(this);
                     mBodyLayout.addView(mMyInfoView.getView());
                 }else{
@@ -179,11 +180,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-    /*
-    * 移除不需要的视图
-    * */
+    //移除不需要的视图
     private void removeAllView() {
-        for(int i = 0;i<mBodyLayout.getChildCount();i++){
+        for (int i=0;i<mBodyLayout.getChildCount();i++){
             mBodyLayout.getChildAt(i).setVisibility(View.GONE);
         }
     }
@@ -192,43 +191,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv_course.setTextColor(Color.parseColor("#666666"));
         tv_exercises.setTextColor(Color.parseColor("#666666"));
         tv_myInfo.setTextColor(Color.parseColor("#666666"));
+
         iv_course.setImageResource(R.drawable.main_course_icon);
         iv_exercises.setImageResource(R.drawable.main_exercises_icon);
         iv_myInfo.setImageResource(R.drawable.main_my_icon);
-        for (int i = 0; i < mBottomLayout.getChildCount(); i++) {
-            mBottomLayout.getChildAt(i).setSelected(false); //设置为未选中，有什么用捏，我不知道
+
+        for (int i=0;i<mBottomLayout.getChildCount();i++){
+            mBottomLayout.getChildAt(i).setSelected(false);
         }
     }
-    protected long exitTime; //上一次点击时间
+
+    protected long exitTime;//记录第一次点击的时间
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction()==KeyEvent.ACTION_DOWN){
-            if(System.currentTimeMillis() - exitTime >2000){
-                Toast.makeText(this, "再点击一次退出应用", Toast.LENGTH_SHORT).show();
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+            if ((System.currentTimeMillis() - exitTime) > 2000){
+                Toast.makeText(this,"再按一次退出博学谷",Toast.LENGTH_SHORT).show();
                 exitTime = System.currentTimeMillis();
             }else{
                 MainActivity.this.finish();
-                if(readLoginStatus()){
+                if (readLoginStatus()){
+                    //已登陆的话，清除登陆状态
                     clearLoginStatus();
                 }
                 System.exit(0);
             }
-            return true; //清除返回按钮事件
+            return true;
         }
         return super.onKeyDown(keyCode, event);
     }
-    /*清除登录状态和登录名，自动填入登录名来源于登录页面的作用于注册的startActivityForResult方法，删除用户名暂时无实际作用，删除状态作用未知*/
+    //清除登陆状态
     private void clearLoginStatus() {
-        SharedPreferences sp = getSharedPreferences("loginInfo",MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean("isLogin",false);   //第二个参数表设置
-        editor.putString("loginUserName","");
-        editor.commit();
+        SharedPreferences sp = getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();//获取编辑器
+        editor.putBoolean("isLogin",false);//清除登陆状态
+        editor.putString("loginUserName","");//清除登陆名
+        editor.commit();//提交修改
     }
 
+    //登陆状态
     private boolean readLoginStatus() {
-        SharedPreferences sp = getSharedPreferences("loginInfo",MODE_PRIVATE);
-        boolean isLogin = sp.getBoolean("isLogin", false); //第二个参数表默认
+        SharedPreferences sp = getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
+        boolean isLogin = sp.getBoolean("isLogin",false);
         return isLogin;
     }
 }
