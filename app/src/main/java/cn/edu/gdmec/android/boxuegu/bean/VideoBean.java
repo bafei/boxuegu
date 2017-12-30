6e1d08d0413199cad12feb8f04f4f06c0b004e1c
SmartinUU <1533204193@qq.com>
2017-12-30 23:52:47 +0800

    视频列表布局

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/bean/VideoBean.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.android.boxuegu.bean;
+
+/**
+ * Created by zhuang zhu on 2017-12-30.
+ */
+
+public class VideoBean {
+    public int chapterId ;//章节id；
+    public int videoId;
+    public String title;  //章节标题
+    public String secondTitle; //视频标题
+    public String videoPath;  // 视频播放地址
+}
