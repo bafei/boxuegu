168842e5eee7034d24f7862c02efd50a527f7314
SmartinUU <1533204193@qq.com>
2017-12-31 11:28:35 +0800

    SQLiteHelper (新增加创建播放记录表)

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/sqlite/SQLiteHelper.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/sqlite/SQLiteHelper.java
@@ -12,8 +12,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class SQLiteHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "bxg.db";
     public static final int DB_VERSION = 1;
-    public static final String U_USERINFO = "userinfo";
-
+    public static final String U_USERINFO = "userinfo";  //个人资料
+    public static final String U_VIDEO_PLAY_LIST = "videoplaylist"; //视频播放列表
 
     public SQLiteHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -31,13 +31,23 @@ public class SQLiteHelper extends SQLiteOpenHelper {
                         +"signature VARCHAR"+" )"
 
         );
-
+        //创建视频播放记录表
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + U_VIDEO_PLAY_LIST+"("
+            +"_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            +"userName VARCHAR,"  //用户名
+            +"chapterId INT"  //章节ID号
+            +"videoId INT,"  //小节ID
+            +"videoPath VARCHAR,"  //视频地址
+            +"title VARCHAR,"  //视频章节名称
+            +"secondTitle VARCHAR,"  //视频名称
+            +")"
+        );
     }
-//数据库升级 数据库版本号增加 升级会调用此方法
+    //数据库升级 数据库版本号增加 升级会调用此方法
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISIS"+U_USERINFO);
+        db.execSQL("DROP TABLE IF EXISTS"+U_USERINFO);
+        db.execSQL("DROP TABLE IF EXISTS"+U_VIDEO_PLAY_LIST);
         onCreate(db);
-
     }
 }
